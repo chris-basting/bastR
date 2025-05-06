d49289c98df8fa15781200e3f271524f4cc7ceec
chris-basting <bast0159@umn.edu>
2025-05-06 00:10:12 -0500

    working on functions

---
+++ R/notin.R
@@ -0,0 +1,5 @@
+#' Not in function
+#' Returns the opposite of the %in% function
+#' Returns true if an element is not in another vector, false if it is in it
+#' @export
+`%notin%` <- Negate(`%in%`)
---
+++ R/palettes.R
@@ -0,0 +1,7 @@
+#' Internal list of custom palettes for plots
+#' @keywords internal
+palettes <- list(
+  rbg = c("#FB0207", "#0096FF", "#008F00"),
+  gbr = c( "#008F00", "#0096FF", "#FB0207"),
+  crayon = c("#FB0207","#FF9300","#0096FF","#008F00", "#9437FF", "#941751", "#011993")
+)
