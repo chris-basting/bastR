7b77403f9b4ce1cbd2d77e260fa42f6fdf86ec76
chris-basting <bast0159@umn.edu>
2025-05-21 22:36:29 -0500

    update boxplot

--- R/bastr_boxplot.R
+++ R/bastr_boxplot.R
@@ -8,7 +8,7 @@
 #' @param stats Data frame containing contrasts and their p-values
 #' @param saveplot True/False as to whether the plot should be saved to path
 #'
-#' @importFrom ggplot2 ggplot
+#' @import ggplot2
 #' @importFrom rstatix get_y_position
 #' @importFrom ggpubr stat_pvalue_manual
 #' @importFrom dplyr left_join
@@ -16,34 +16,46 @@
 #'
 
 
-library(ggplot2)
-library(rstatix)
-library(ggprism)
-library(dplyr)
-library(ggpubr)
+#library(ggplot2)
+#library(rstatix)
+#library(ggprism)
+#library(dplyr)
+#library(ggpubr)
 
 
-bastr_boxplot <- function(x, var, group, title = NULL, stats = NULL, path = getwd()){
+bastr_boxplot <- function(x, var, group, title = NULL, stats = NULL, save.plot = F, path = getwd(), group.colors = NULL){
 p <- ggplot(data = x, aes(x=group, y=var)) +
        geom_boxplot(outlier.shape = NA, aes(color = group, fill = group)) +
        geom_jitter(width = .2) +
        theme_prism() +
-  scale_colour_prism(palette = "floral") +
-  scale_fill_prism(palette = "floral") +
-  stat_pvalue_manual(data = positions)
+       ggtitle(title)
 
 # If title is provided, add labels
-if (!is.null(title)) {
+#if (!is.null(title)) {
+#  p <- p + ggtitle(title)
+#  }
+
+# If colors aren't provided use default, otherwise use specified group colors
+if (is.null(group.colors)) {
+    p <- p + scale_colour_prism(palette = "colors") +
+             scale_fill_prism(palette = "colors")
+
+  } else(
+   p <- p
+)
+
+
+# If stats are provided, add p values
+if (!is.null(stats)) {
+
+  positions <- get_y_position(x, formula = var ~ group) %>%
+    right_join(stats, by = c("group1", "group2"))
   p <- p + ggtitle(title)
-}
+  }
 
 }
 
 
-test <- data.frame(contrast= c("setosa - versicolor", "setosa - virginica", "versicolor - virginica"),
-                   group1 = c("setosa", "setosa", "versicolor"),
-                   group2 = c("versicolor", "virginica", "virginica"),
-                   p.adj = c(0.05, 0.01, 0.1))
 
-positions <- get_y_position(iris, formula = Sepal.Length ~ Species) %>%
-  right_join(test, by = c("group1", "group2"))
+
+
