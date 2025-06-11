c47d1adc838d3f74155fccadb9a8d1bbaa237905
chris-basting <bast0159@umn.edu>
2025-06-10 23:48:48 -0500

    added is zero or na function. added replace missing function. tidied up

---
+++ R/replace_missing.R
@@ -0,0 +1,21 @@
+#' Replaces missing (NA) values in given columns of a dataframe.
+#' @export
+#' @param x Dataframe containing the columns with missing values to be replaced
+#' @param cols Vector of column names (strings) to have missing values replaced
+#' @param method Method for replacing missing values. Default is lowest value in the column divided by 2
+#'
+#' @importFrom dplyr mutate_at
+
+
+replace_missing <- function(x, cols, method = "lod2"){
+
+    if(method == "lod2"){
+    val <- mutate_at(x, vars(cols), ~replace(., is_zero_or_na(.), min(.[.>0], na.rm = TRUE)/2))
+    }
+
+   if(method == "lod5"){
+    val <- mutate_at(x, vars(cols), ~replace(., is_zero_or_na(.), min(.[.>0], na.rm = TRUE)/5))
+   }
+    return(val)
+
+}
--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,22 @@
-#' @importFrom magrittr %>%
+#' Pipe operator
+#'
+#' See \code{magrittr::\%>\%} for details.
+#'
 #' @name %>%
 #' @rdname pipe
+#' @importFrom magrittr %>%
+#' @export
 NULL
+
+#' Return True/False depending on if value is zero or NA
+#'
+#' @param x A vector
+#' @return A boolean vector of whether each index is a zero or NA
+#' @export
+#'
+is_zero_or_na <- function(x) {
+ifelse(x == 0 | is.na(x), T, F)
+}
+#'
+#'
+#'
