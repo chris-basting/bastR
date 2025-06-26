29e01a59c1c70f4aff89ede1ebc51e0bd9f46306
chris-basting <bast0159@umn.edu>
2025-06-25 23:46:18 -0500

    working on lm function

--- R/bastr_lm.R
+++ R/bastr_lm.R
@@ -8,6 +8,46 @@
 
 bastr_lm <- function(x, vars, formula, check_model = T){
 
+  setClass("MyObject",
+           slots = list(
+             results = "data.frame",
+             models = "list"
+           ))
+
+
+  model.list <- list()  # store your models
+  results <- list() # store each models results
+
+  for (i in c(vars)) {  # loop through predictors
+
+    # Build formula safely using backticks
+    formula <- as.formula(paste0("`",var,"`"," ~ ", formula))
+
+    # Fit model
+    model <- lmerTest::lmer(formula, data = x)
+
+    #Add model to list of models
+    model.list[[var]] <- model
+
+    #Tidy the model output
+    model_tidy <- broom::tidy(model) %>%
+      filter(term != "(Intercept)")  # keep only the predictor terms
+
+    # Get R-squared
+    r_squared <- summary(model)$r.squared
+
+    # Save results
+    results[[var]] <- model_tidy %>%
+      mutate(R2 = r_squared,
+             Variable = var)
+
+
+  }
+
+  # Combine all results into a dataframe
+  result.df <- bind_rows(results)
+
+  S4.return <- new("MyObject", results = result.df, models = model.list)
 
 }
 
